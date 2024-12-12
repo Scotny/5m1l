d8add84292bb9e4acd66d67b68abdd710ec4196a
Behruz <marsithb2022@gmail.com>
2024-12-12 19:33:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let ism = prompt("Ismingizni kiriting")
+console.log("Ism: " + ism);
+
+let surname = prompt("Familiyangizni kiriting")
+console.log("Familiya: " + surname);
+
+let age = prompt("Yoshingizni kiriting")
+console.log("Yosh: " + age);
+
+let lesson = prompt("Yoqtirgan faningizni kiriting")
+console.log("Yoqtirgan fan: " + lesson);
+
+let place = prompt("Yashaydigan tumaningizni kiriting")
+console.log("Yashash manzili: " + place);
